fc09c19cb821b8133b8c710b2380dee4ca561ad7
ItzJerry <85010676+ItzJerry317@users.noreply.github.com>
2024-05-27 22:28:14 +0800

    ncmsong function

--- src/index.js
+++ src/index.js
@@ -3,6 +3,23 @@ const chalk = require("chalk")
 const DiscordRPC = require('discord-rpc');
 const hmc = require('hmc-win32');
 const clientId = '1137949966921371738';
+const ncm = require("./ncm.js")
+
+//ncm song fetch
+var ncmpid = ncm.getNCMPid()
+var ncmSong = ncm.getSong(ncmpid)
+var songname = ncmSong.split(" - ")
+console.log(chalk.yellow(ncmSong))
+
+//rpc detail, state管理
+var state, detail
+if (ncmSong == "网易云音乐"){
+  state = "Finding music"
+  detail = "hmm"
+} else {
+  state = "Listening to music"
+  detail = ncmSong
+}
 
 //rich presence initialization
 DiscordRPC.register(clientId);
@@ -12,22 +29,17 @@ var startTimestamp = new Date();
 
 async function setActivity() {
   rpc.setActivity({
-    details: `ok`,
-    state: 'Listening to music',
+    details: detail,
+    state: state,
     startTimestamp,
-    largeImageKey: 'netease',
-    largeImageText: 'wow',
+    largeImageKey: 'https://p2.music.126.net/anGmYEbxBunenbY3cuLm_w==/109951169518974669.jpg',
+    largeImageText: detail,
     instance: false,
   });
 }
 
 rpc.on('ready', () => {
   setActivity();
-
-  // activity can only be set every 15 seconds
-  setInterval(() => {
-    setActivity();
-  }, 15e3);
 });
 
 rpc.login({ clientId }).catch(console.error);
